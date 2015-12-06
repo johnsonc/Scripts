019642426fdb927bdd2a0e29c10565e0547c07e3
akshay bhat <akshayubhat@gmail.com>
2015-12-05 19:57:56 -0500

    Added Swift Playground

---
+++ AkshatLearnsSwift.playground/Contents.swift
@@ -0,0 +1,17 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+var test = [1,2,23]
+for (i,k) in test.enumerate(){
+    print(i)
+}
+
+let dsa = test.count;
+print(dsa)
+let asd = test.capacity;
+
+class First{
+    
+}
